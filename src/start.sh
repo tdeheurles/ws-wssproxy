3d5824f08c52d295ad4661423f17020e8c6fcf1b
tdeheurles <tdeheurles@gmail.com>
2015-06-30 17:49:50 +0200

    Change configuration

--- src/start.sh
+++ src/start.sh
@@ -3,19 +3,19 @@
 # If the SERVICE_HOST_ENV_NAME and SERVICE_PORT_ENV_NAME vars are provided,
 # they point to the env vars set by Kubernetes that contain the actual
 # target address and port. Override the default with them.
-if [ -n "${SERVICE_HOST_ENV_NAME+1}" ]; then
-  TARGET_SERVICE=${!SERVICE_HOST_ENV_NAME}
-fi
-if [ -n "${SERVICE_PORT_ENV_NAME+1}" ]; then
-  TARGET_SERVICE="$TARGET_SERVICE:${!SERVICE_PORT_ENV_NAME}"
-fi
-
-echo "SERVICE_PORT_ENV_NAME: $SERVICE_PORT_ENV_NAME"
-echo "SERVICE_HOST_ENV_NAME: $SERVICE_HOST_ENV_NAME"
-echo "TARGET_SERVICE: $TARGET_SERVICE"
+# if [ -n "${SERVICE_HOST_ENV_NAME+1}" ]; then
+#   TARGET_SERVICE=${!SERVICE_HOST_ENV_NAME}
+# fi
+# if [ -n "${SERVICE_PORT_ENV_NAME+1}" ]; then
+#   TARGET_SERVICE="$TARGET_SERVICE:${!SERVICE_PORT_ENV_NAME}"
+# fi
+#
+# echo "SERVICE_PORT_ENV_NAME: $SERVICE_PORT_ENV_NAME"
+# echo "SERVICE_HOST_ENV_NAME: $SERVICE_HOST_ENV_NAME"
+# echo "TARGET_SERVICE: $TARGET_SERVICE"
 
 # Tell nginx the address and port of the service to proxy to
-sed -i "s/{{TARGET_SERVICE}}/${TARGET_SERVICE}/g;" /etc/nginx/conf.d/site.conf
+# sed -i "s/{{TARGET_SERVICE}}/${TARGET_SERVICE}/g;" /etc/nginx/conf.d/site.conf
 
 echo "Starting nginx..."
 nginx -g 'daemon off;'
